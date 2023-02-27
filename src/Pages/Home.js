d2c265588c3204ab5ae8946e7e341d4338cccf56
athulssiorbit <athulss@iorbit-tech.com>
2023-02-27 13:14:22 +0530

    list add

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Home = ({setListData}) => {
-  const nav = useNavigate()
+const Home = ({ setListData }) => {
+  const nav = useNavigate();
   const [list, setList] = useState([]);
   useEffect(() => {
     axios
@@ -16,30 +16,52 @@ const Home = ({setListData}) => {
 
   const handleClink = (item) => {
     //console.log(item);
-    setListData(item)
-    nav('/main')
-  }
+    setListData(item);
+    nav("/main");
+  };
+
+  const handleEditClick = () => {
+    //console.log(item);
+    nav("/create");
+  };
 
   return (
-    <div style={{ height: "100%", width: "100%" }}>
-      {list.map((item,index) => {
-        return (
-          <div
-          key={index}
-          onClick={()=>handleClink(item)}
-            style={{
-              margin: "12px",
-              padding: "12px",
-              width: "85%",
-              backgroundColor: "yellowgreen",
-            }}
-          >
-           {
-            item.name
-           }
-          </div>
-        );
-      })}
+    <div style={{ height: "60%", width: "100%" }}>
+      <div style={{ height: "100%", width: "100%" }}>
+        {list.map((item, index) => {
+          return (
+            <div
+              key={index}
+              onClick={() => handleClink(item)}
+              style={{
+                // margin: "12px",
+                // padding: "12px",
+                height: "42px",
+                width: "100%",
+                backgroundColor: "yellowgreen",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {item.name}
+            </div>
+          );
+        })}
+      </div>
+      <div
+        onClick={handleEditClick}
+        style={{
+          height: "42px",
+          width: "100%",
+          backgroundColor: "aliceblue",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        ADD LIST
+      </div>
     </div>
   );
 };
